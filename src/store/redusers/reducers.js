7d391e3d059c7f7d4346f2a6074da0d3841d37da
Lemmiko <61229310+Lemmiko@users.noreply.github.com>
2021-07-26 11:36:09 +0300

    reducer

--- src/store/redusers/reducers.js
+++ src/store/redusers/reducers.js
@@ -20,26 +20,28 @@ import {ADD_COMPONENT} from '../ActionCreators/action'
 const reducerAddComponent = (state = initialState, action) => {
     switch(action.type) {
         case ADD_COMPONENT: 
-                console.log(state.currentComponents[1].title)
-            return  {...state, 
-                currentComponents: [
-                    {title: action.name,
-                    key: 1},
-                    state.currentComponents
-                    ] 
-                };   
+        if (!state.currentComponents[0].title) {
+            console.log(state.currentComponents[0])
+            return {...state,
+                    currentComponents: [
+                        {title: action.name,
+                        key: 1},
+                        state.currentComponents
+                            ]
+                        };
+                    } else if (!state.currentComponents[1].title){
+                        console.log(state.currentComponents[1]) 
+                        return {...state,
+                            currentComponents: [
+                                state.currentComponents[0],
+                                {title: action.name,
+                                key: 2}]
+                                } 
+                    } return state              
         default:
-            console.log(state.currentComponents.title)
              return state 
         } 
 };
 
 export default reducerAddComponent;
-
-// return  {...state, 
-//     currentComponents: [
-//         {title: action.name,
-//         key: 1},
-//         state.currentComponents
-//         ]
-//     }
+  
